cdfe615a93fc0f6d0f97f5739d36b303c13798e2
Dom <szborzdynski@gmail.com>
2024-10-23 16:40:41 +0200

    dodano metody calcualte pathj len oraz print best solution

--- src/genetic.rs
+++ src/genetic.rs
@@ -1,7 +1,8 @@
+use crate::conf::Config;
+use crate::osobnik::Solution;
 
-
-fn print_best(){
-
+fn print_best(config: Config){
+    println!("best solution {:?}", config.best_solution.cities);
 }
 
 fn timer(){
@@ -12,8 +13,15 @@ fn generate_starting_population(){
 
 }
 
-fn calculate_path_length(){
-
+pub(crate) fn calculate_path_length(solution: &mut Solution, config: &mut Config) -> i32 { //todo test this
+    let mut sum_path: i32 = 0;
+    let cities_count = solution.cities.len() - 1;
+    for i in (0..cities_count){
+        sum_path = sum_path + config.matrix[solution.cities[i] as usize][solution.cities[i + 1] as usize];
+    }
+    // go back to first city
+    sum_path += config.matrix[solution.cities[cities_count] as usize][solution.cities[0] as usize];
+    return sum_path;
 }
 
 fn evaluate_population(){
